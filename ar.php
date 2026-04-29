35d290fb51317c27a68096e307ae65cd41d8218e
mathis klein <mathisklein33@gmail.com>
2026-04-29 16:34:30 +0200

    améllioration du ar avec orbite et disperssion et meillleur vissibiliter

--- ar.php
+++ ar.php
@@ -15,76 +15,46 @@
 
     <a-marker preset="hiro">
 
-        <a-sphere
-                position="0 0.4 0"
-                radius="0.25"
-                color="yellow">
-        </a-sphere>
+        <a-entity position="0 0.15 0" rotation="0 0 0" scale="0.45 0.45 0.45">
 
-        <a-sphere
-                position="0.45 0.25 0"
-                radius="0.05"
-                color="gray">
-        </a-sphere>
 
-        <a-sphere
-                position="0.65 0.25 0"
-                radius="0.08"
-                color="orange">
-        </a-sphere>
 
-        <a-entity
-                gltf-model="https://solarsystem.nasa.gov/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaTBSIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7c1183d35fdc9e4b5143c8601376552b89b5d99f/Earth_1_12756.glb"
-                position="0.9 0.25 0"
-                scale="0.015 0.015 0.015">
+            <a-torus position="0 0 0" rotation="90 0 0" radius="0.45" radius-tubular="0.003" color="white"></a-torus>
+            <a-torus position="0 0 0" rotation="90 0 0" radius="0.65" radius-tubular="0.003" color="white"></a-torus>
+            <a-torus position="0 0 0" rotation="90 0 0" radius="0.85" radius-tubular="0.003" color="white"></a-torus>
+            <a-torus position="0 0 0" rotation="90 0 0" radius="1.05" radius-tubular="0.003" color="white"></a-torus>
+            <a-torus position="0 0 0" rotation="90 0 0" radius="1.3" radius-tubular="0.003" color="white"></a-torus>
+            <a-torus position="0 0 0" rotation="90 0 0" radius="1.55" radius-tubular="0.003" color="white"></a-torus>
+            <a-torus position="0 0 0" rotation="90 0 0" radius="1.75" radius-tubular="0.003" color="white"></a-torus>
+
+            <a-sphere position="0 0 0" radius="0.22" color="yellow"></a-sphere>
+
+            <a-sphere position="0.45 0 0" radius="0.04" color="gray"></a-sphere>
+
+            <a-sphere position="0.46 0 0.46" radius="0.06" color="orange"></a-sphere>
+
+            <a-entity
+                    gltf-model="https://solarsystem.nasa.gov/rails/active_storage/blobs/redirect/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBaTBSIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--7c1183d35fdc9e4b5143c8601376552b89b5d99f/Earth_1_12756.glb"
+                    position="0 0 0.85"
+                    scale="0.012 0.012 0.012">
+            </a-entity>
+
+            <a-sphere position="-0.74 0 0.74" radius="0.05" color="red"></a-sphere>
+
+            <a-sphere position="-1.3 0 0" radius="0.14" color="#c49a6c"></a-sphere>
+
+            <a-sphere position="-1.1 0 -1.1" radius="0.11" color="#d8c28a"></a-sphere>
+            <a-torus position="-1.1 0 -1.1" rotation="90 0 0" radius="0.16" radius-tubular="0.008" color="#e6d8a8"></a-torus>
+
+            <a-sphere position="0 0 -1.75" radius="0.09" color="#7fdfff"></a-sphere>
+
+            <a-sphere position="1.24 0 -1.24" radius="0.09" color="blue"></a-sphere>
+
+            <a-sphere position="1.75 0 0" radius="0.035" color="#b8a28a"></a-sphere>
+
         </a-entity>
 
-        <a-sphere
-                position="1.15 0.25 0"
-                radius="0.07"
-                color="red">
-        </a-sphere>
-
-        <a-sphere
-                position="1.5 0.3 0"
-                radius="0.17"
-                color="#c49a6c">
-        </a-sphere>
-
-        <a-sphere
-                position="1.9 0.3 0"
-                radius="0.14"
-                color="#d8c28a">
-        </a-sphere>
-
-        <a-torus
-                position="1.9 0.3 0"
-                rotation="90 0 0"
-                radius="0.2"
-                radius-tubular="0.01"
-                color="#e6d8a8">
-        </a-torus>
-
-        <a-sphere
-                position="2.25 0.25 0"
-                radius="0.11"
-                color="#7fdfff">
-        </a-sphere>
-
-        <a-sphere
-                position="2.55 0.25 0"
-                radius="0.11"
-                color="blue">
-        </a-sphere>
-
-        <a-sphere
-                position="2.85 0.25 0"
-                radius="0.04"
-                color="#b8a28a">
-        </a-sphere>
-
     </a-marker>
-
     <a-entity camera></a-entity>
 
 </a-scene>
