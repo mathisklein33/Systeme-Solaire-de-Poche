5e44283b4b2fc304d0154a8f733452b243efe6fb
mathis klein <mathisklein33@gmail.com>
2026-05-04 09:41:21 +0200

    annimation du systéme solaire

--- ar.php
+++ ar.php
@@ -3,7 +3,7 @@
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
-    <title>Système solaire AR</title>
+    <title>Système solaire AR animé</title>
 
     <script src="https://aframe.io/releases/1.4.0/aframe.min.js"></script>
     <script src="https://cdn.jsdelivr.net/gh/AR-js-org/AR.js/aframe/build/aframe-ar.js"></script>
@@ -27,6 +27,7 @@
 
         <a-entity position="0 0.08 0" scale="0.35 0.35 0.35">
 
+            <!-- Orbites -->
             <a-torus rotation="90 0 0" radius="0.45" radius-tubular="0.003" color="white"></a-torus>
             <a-torus rotation="90 0 0" radius="0.65" radius-tubular="0.003" color="white"></a-torus>
             <a-torus rotation="90 0 0" radius="0.85" radius-tubular="0.003" color="white"></a-torus>
@@ -35,26 +36,61 @@
             <a-torus rotation="90 0 0" radius="1.55" radius-tubular="0.003" color="white"></a-torus>
             <a-torus rotation="90 0 0" radius="1.75" radius-tubular="0.003" color="white"></a-torus>
 
-            <a-sphere position="0 0 0" radius="0.22" color="yellow"></a-sphere>
+            <!-- Soleil animé -->
+            <a-sphere
+                    position="0 0 0"
+                    radius="0.22"
+                    color="yellow"
+                    animation="property: rotation; to: 0 360 0; loop: true; dur: 8000; easing: linear">
+            </a-sphere>
 
-            <a-sphere position="0.45 0 0" radius="0.04" color="gray"></a-sphere>
-            <a-sphere position="0.46 0 0.46" radius="0.06" color="orange"></a-sphere>
-
-            <a-entity
-                    gltf-model="https://modelviewer.dev/shared-assets/models/Earth.glb"
-                    position="0 0 0.85"
-                    scale="0.012 0.012 0.012">
+            <!-- Mercure -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 4000; easing: linear">
+                <a-sphere position="0.45 0 0" radius="0.04" color="gray"></a-sphere>
             </a-entity>
 
-            <a-sphere position="-0.74 0 0.74" radius="0.05" color="red"></a-sphere>
-            <a-sphere position="-1.3 0 0" radius="0.14" color="#c49a6c"></a-sphere>
+            <!-- Vénus -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 7000; easing: linear">
+                <a-sphere position="0.65 0 0" radius="0.06" color="orange"></a-sphere>
+            </a-entity>
 
-            <a-sphere position="-1.1 0 -1.1" radius="0.11" color="#d8c28a"></a-sphere>
-            <a-torus position="-1.1 0 -1.1" rotation="90 0 0" radius="0.16" radius-tubular="0.008" color="#e6d8a8"></a-torus>
+            <!-- Terre -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 10000; easing: linear">
+                <a-entity
+                        gltf-model="#"
+                        position="0.85 0 0"
+                        scale="0.012 0.012 0.012"
+                        animation="property: rotation; to: 0 360 0; loop: true; dur: 2500; easing: linear">
+                </a-entity>
+            </a-entity>
 
-            <a-sphere position="0 0 -1.75" radius="0.09" color="#7fdfff"></a-sphere>
-            <a-sphere position="1.24 0 -1.24" radius="0.09" color="blue"></a-sphere>
-            <a-sphere position="1.75 0 0" radius="0.035" color="#b8a28a"></a-sphere>
+            <!-- Mars -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 13000; easing: linear">
+                <a-sphere position="1.05 0 0" radius="0.05" color="red"></a-sphere>
+            </a-entity>
+
+            <!-- Jupiter -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 18000; easing: linear">
+                <a-sphere position="1.30 0 0" radius="0.14" color="#c49a6c"></a-sphere>
+            </a-entity>
+
+            <!-- Saturne -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 23000; easing: linear">
+                <a-entity position="1.55 0 0">
+                    <a-sphere radius="0.11" color="#d8c28a"></a-sphere>
+                    <a-torus rotation="90 0 0" radius="0.16" radius-tubular="0.008" color="#e6d8a8"></a-torus>
+                </a-entity>
+            </a-entity>
+
+            <!-- Uranus -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 28000; easing: linear">
+                <a-sphere position="1.55 0 0" radius="0.09" color="#7fdfff"></a-sphere>
+            </a-entity>
+
+            <!-- Neptune -->
+            <a-entity animation="property: rotation; to: 0 360 0; loop: true; dur: 33000; easing: linear">
+                <a-sphere position="1.75 0 0" radius="0.09" color="blue"></a-sphere>
+            </a-entity>
 
         </a-entity>
 
